01ef3e93caf33663b98f605bab471c5399cbff67
Ben Dodd <ben-dodd@outlook.com>
2022-12-28 12:05:10 +0000

    post design fixes

--- app/src/main/java/com/mitgobla/newsaggregator/MainActivity.kt
+++ app/src/main/java/com/mitgobla/newsaggregator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mitgobla.newsaggregator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.mainToolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate((R.menu.toolbar_front_page), menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
